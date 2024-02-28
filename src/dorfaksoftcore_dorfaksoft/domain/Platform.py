f230842782ee98c154c0356cfe91bb1286f6efb5
HESAM <h.mirazimi@gmail.com>
2024-02-28 12:34:37 +0330

    pypi init

--- src/dorfaksoftcore_dorfaksoft/domain/Platform.py
+++
@@ -1,5 +0,0 @@
-class Platform():
-    Web = "Web"
-    Android="Android"
-    Ios = "Ios"
-
--- src/dorfaksoftcore_dorfaksoft/infrastructure/UploadHelper.py
+++
@@ -1,9 +0,0 @@
-class UploadHelper():
-    @staticmethod
-    def allowedFile(filename, validExtension=None):
-        return '.' in filename and \
-               filename.rsplit('.', 1)[1].lower() in validExtension
-
-    @staticmethod
-    def makeOutputFormat( value):
-        return "<html><body><div id='res' >" + value + "</div></body></html>"
